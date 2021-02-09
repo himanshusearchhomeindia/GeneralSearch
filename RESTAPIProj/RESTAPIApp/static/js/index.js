cabee475f796ccfa19375c9bf25d169ed3ad311e
Himanshu's PC <himanshusearchhomeindia@gmail.com>
2021-02-09 11:32:50 +0530

    Front end added

--- RESTAPIProj/RESTAPIApp/static/js/index.js
+++ RESTAPIProj/RESTAPIApp/static/js/index.js
@@ -1,6 +1,8 @@
 const SearchForm = document.getElementById('SearchForm'); //Accessing the form to add eventlistener and send API request.
 const SearchQuery = document.getElementById('query'); //Accessing the user query from the input tab.
 const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value; //accessing the crsf token.
+const PropertyContainer = document.getElementById('PropertyContainer');  //Accessing the property element to insert the filtered properties in it.
+
 
 //listening to the form submission event.
 SearchForm.addEventListener('submit', (event) => {
@@ -23,10 +25,28 @@ SearchForm.addEventListener('submit', (event) => {
         }
     };
     fetch(url, options) //fetch request.
-    .then(Response => Response.json())//here we are converting the data into json format.
-    .then(data => {
-            //this function will show the results in the console.
+        .then(Response => Response.json())//here we are converting the data into json format.
+        .then(data => {
             console.log(data);
-    })
-    .catch(error => console.log(error)); //if error's came it will appear into the console.
+            // this function will show the results in the console.
+            let html = "";
+            data.forEach(element => {
+                html += `
+                <div class="d-flex flex-row p-2 bd-highlight card my-4" style="width: 70rem;">
+                    <img src="${element.Proprety_Image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.PropertyName}</h5><span class="badge bg-secondary">Avaliable for:${element.Avaliable_For}</span>
+                        <p class="card-text">PropertyInfo:-${element.Property_Description}</p>
+                        <p class="card-text">PropertyAddress:-${element.PropertyAddress}</p>
+                        <p class="card-text">Builder:-${element.BuilderName}</p>
+                        <p class="card-text">Type:-${element.Property_Type}</p>
+                        <p class="card-text">Price:-${element.Property_Price}</p>
+                        <p class="card-text">Status:-${element.PropertyStatus}</p>
+                    </div>
+                </div>`
+            });
+            PropertyContainer.innerHTML += html;
+
+        })
+        .catch(error => console.log(error)); //if error's came it will appear into the console.
 });
--- RESTAPIProj/RESTAPIProj/settings.py
+++ RESTAPIProj/RESTAPIProj/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os #for setting the static files.
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static') #for adding the static files in the page.
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -126,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
